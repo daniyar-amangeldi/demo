a258d06bd3e0df12df3603f987a26d164ee2e825
daniyar.amangeldy <d.amangeldy@aviata.me>
2025-03-16 01:32:32 +0500

    init clean architecture

--- app/src/main/java/com/example/demo/di/MovieModule.kt
+++ app/src/main/java/com/example/demo/di/MovieModule.kt
@@ -1,46 +1,76 @@
 package com.example.demo.di
 
-import androidx.room.Room
-import com.example.demo.App
-import com.example.demo.model.api.MovieApi
-import com.example.demo.model.dao.MovieDao
-import com.example.demo.model.datasource.ApiSource
-import com.example.demo.model.db.AppDatabase
+import com.example.data.repository.MovieRepositoryImpl
+import com.example.data.source.remote.api.MovieApi
+import com.example.data.source.remote.MovieRemoteDataSource
+import com.example.data.util.network.NetworkConfig
+import com.example.data.util.network.NetworkManager
+import com.example.data.source.local.DatabaseProvider
+import com.example.data.source.local.DatabaseProviderImpl
+import com.example.data.source.local.dao.MovieDao
 import com.example.demo.viewmodel.MovieDetailsViewModel
 import com.example.demo.viewmodel.MovieViewModel
+import com.example.domain.repository.MovieRepository
+import com.example.domain.usecase.GetMovieList
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
-private const val DATABASE_NAME = "demo_database"
+private const val AUTHORIZATION_TOKEN =
+    "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiODJjMTcyZTdiZjY2NjA1MTY4ODFjNmExZWQ2MTZkZCIsIm5iZiI6MTczMDU0MjY1MS41NDc5MDIzLCJzdWIiOiI1ZjkxYjEzYTU1YzFmNDAwMzkyNzk5YTIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.JQN6zlqcT5VT4E8HxJz_sytmEU9lQELBsRU5s4SEFXk"
 
 val movieModule = module {
     viewModel {
-        MovieDetailsViewModel(
-            movieDao = get<MovieDao>()
-        )
+        MovieDetailsViewModel()
     }
 
     viewModel {
         MovieViewModel(
-            movieDao = get<MovieDao>(),
-            client = get<MovieApi>()
+            getMovieList = get<GetMovieList>()
         )
     }
 
-    single<MovieDao> {
-        get<AppDatabase>().movieDao()
+    factory<GetMovieList> {
+        GetMovieList(
+            repository = get<MovieRepository>()
+        )
+    }
+
+    single<MovieRepository> {
+        MovieRepositoryImpl(
+            remoteDataSource = get<MovieRemoteDataSource>()
+        )
+    }
+
+    single<MovieRemoteDataSource> {
+        MovieRemoteDataSource(
+            api = get<MovieApi>()
+        )
     }
 
     single<MovieApi> {
-        ApiSource.retrofit.create(MovieApi::class.java)
+        NetworkManager.createService(
+            config = get<NetworkConfig>()
+        )
     }
 
-    single<AppDatabase> {
-        Room.databaseBuilder(
-            androidContext(),
-            AppDatabase::class.java,
-            DATABASE_NAME
-        ).build()
+    single<NetworkConfig> {
+        NetworkConfig(
+            baseUrl = "https://api.themoviedb.org/",
+            timeout = 60L,
+            headers = hashMapOf(
+                "Authorization" to "Bearer $AUTHORIZATION_TOKEN"
+            ),
+            interceptors = listOf()
+        )
+    }
+
+//    single<MovieDao> {
+//        val databaseProvider = get<DatabaseProvider>()
+//        databaseProvider.getDatabase().movieDao()
+//    }
+
+    single<DatabaseProvider> {
+        DatabaseProviderImpl(androidContext())
     }
 }
